6a45db708a2f9cc6e2aa6449cc7f6def21def4ef
novandafadly <>
2022-04-17 01:44:35 +0000

    generate scaffold MenuItem

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,8 +1,2 @@
 class MenuItem < ApplicationRecord
-  validates :name, presence:true, uniqueness:true
-  validates :price, presence:true, numericality: {greater_than: 0.01}
-  validates :description, presence:true, length: {maximum: 150}
-
-has_many :categories
-  
 end
---
+++ spec/factories/menu_items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :menu_item do
+    name { "MyString" }
+    price { 1.5 }
+    description { "MyText" }
+  end
+end
